c44b04a3a6975a1c2627e7b1f1d549033a7d0516
caposseleDigicat <angelo.capossele@digicatapult.org.uk>
2018-09-25 12:01:59 +0100

    unit test post added

--- tests/test.js
+++ tests/test.js
@@ -14,11 +14,32 @@ describe('Tests app', function() {
         done(err);
     });
   });
-  // it('verifies post', function(done) {
-  //   request.post('/ttn').expect(201).end(function(err, result) {
-  //       //test.string(result.body.Body).contains('test');
-  //       //test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
-  //       done(err);
-  //   });
-  // });
+  it('verifies post', function(done) {
+    var payload = {
+        "app_id" : "everydayiot",
+        "dev_id": "temperature1",
+        "hardware_serial": "4883C7DF30051783",
+        "port": 25,
+        "counter": 41,
+        "payload_raw": "VDoyMi41Ng==",
+        "metadata":
+          { 
+            "time": "2018-09-24T16:29:07.886534138Z",
+            "frequency": 867.1,
+            "modulation": "LORA",
+            "data_rate": "SF12BW125",
+            "coding_rate": "4/5",
+            "gateways": [{
+              "latitude": 0.0,
+              "longitude": 0.0
+            }]
+          },
+        "downlink_url": "https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/everydayiot/marketplace?key=ttn-account-v2.RpbgPHgq3lH82QJGC_2g1WEnX2ossRDVmH-WgPMfA-Q" 
+    }
+    request.post('/ttn').send(payload).expect(201).end(function(err, result) {
+        //test.string(result.body.Body).contains('test');
+        //test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
+        done(err);
+    });
+  });
 });
