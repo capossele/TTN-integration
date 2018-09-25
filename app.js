3a9514c378037da035f179ce35a2a25add64bba8
caposseleDigicat <angelo.capossele@digicatapult.org.uk>
2018-09-25 10:12:01 +0100

    body

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 var express = require('express');
 var app = express();
-const path = require('path');
 const bodyParser = require('body-parser');
-//const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
-const router = express.Router();
 
-//app.use(awsServerlessExpressMiddleware.eventContext())
-router.use(bodyParser.json())
+app.use(bodyParser.json())
 
-router.get('/', function(req, res) {
+app.get('/', function(req, res) {
   res.send({
     "Output": "Hello World!"
   });
 });
 
-router.post('/testPost', function(req, res) {
+app.post('/testPost', function(req, res) {
   res.status(201).json({
     "Body": req.body,
     "Headers": req.headers
   });
 });
 
-app.use('/', router)
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
